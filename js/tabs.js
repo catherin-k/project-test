047bd80f64188e34b5221151d4a8a65596e633d1
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-09-22 22:50:55 +0300

    add smothScroll & tabs

---
+++ js/tabs.js
@@ -0,0 +1,20 @@
+const tabsHandlerElems = document.querySelectorAll("[data-tabs-handler]");
+const tabsContentElems = document.querySelectorAll("[data-tabs-field]");
+console.log(tabsContentElems);
+
+for (let btn of tabsHandlerElems) {
+  btn.addEventListener("click", () => {
+    tabsHandlerElems.forEach((item) =>
+      item.classList.remove("design-list__item_active")
+    );
+    btn.classList.add("design-list__item_active");
+
+    tabsContentElems.forEach((element) => {
+      if (element.dataset.tabsField === btn.dataset.tabsHandler) {
+        element.classList.remove("hidden");
+      } else {
+        element.classList.add("hidden");
+      }
+    });
+  });
+}
